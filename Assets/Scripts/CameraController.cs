48ef9bf7d1583a96e8975f247f90b51b7253ffd9
Patryk Urban <p.urban@kainos.com>
2017-07-22 03:29:54 +0200

    Working scrolling scenes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        public int NumberOfScenes;
+        private int _currentScene = 1;
+
+        public float ScreenScrollOffset = 13;
+        private float _targetCameraPositionX;
+
+        public float SmoothTimeX;
+        private Vector2 _velocity;
+
+        public GameObject Hud;
+
+        public void ScrollRight()
+        {
+            if (_currentScene < NumberOfScenes)
+            {
+                _targetCameraPositionX += ScreenScrollOffset;
+                _currentScene++;
+            }
+        }
+
+        public void ScrollLeft()
+        {
+            if (_currentScene >= 2)
+            {
+                _targetCameraPositionX -= ScreenScrollOffset;
+                _currentScene--;
+            }
+        }
+
+        private void Update()
+        {
+            var posX = Mathf.SmoothDamp(transform.position.x, _targetCameraPositionX, ref _velocity.x, SmoothTimeX);
+
+            transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+            Hud.transform.position = new Vector2(posX, transform.position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/LeftScreenScroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LeftScreenScroller : MonoBehaviour
+    {
+        public CameraController CameraController;
+
+        void OnMouseOver()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                CameraController.ScrollLeft();
+            }
+        }
+    }
+}
