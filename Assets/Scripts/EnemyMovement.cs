4f542822e904b259bbc754d141c21dcda0a0dab3
Patryk Urban <p.urban@kainos.com>
2017-07-23 08:44:02 +0200

    Working enemy script

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        public SpriteRenderer Renderer;
+        public GameObject[] Waypoints;
+
+        private Vector3 _currentTarget;
+        private int _waypointIndex;
+        public float Speed;
+
+        void Start ()
+        {
+            _currentTarget = Waypoints[_waypointIndex++].transform.position;
+        }
+
+        void Update ()
+        {
+            if (Vector3.Distance(transform.position, _currentTarget) < 0.5f)
+            {
+                _currentTarget = Waypoints[_waypointIndex++].transform.position;
+                Renderer.flipX = _waypointIndex%2 == 0;
+            }
+
+            if (_waypointIndex >= Waypoints.Length)
+            {
+                _waypointIndex = 0;
+            }
+            
+            var m = Vector3.MoveTowards(transform.position, _currentTarget, Speed * Time.deltaTime);
+            transform.position = m;
+        }
+
+        void OnMouseEnter()
+        {
+            Debug.Log("JUMP SCARE");
+        }
+    }
+}
